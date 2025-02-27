8bd76fc1bff529cc4801268075d23e9c91cadda5
Ray <nikanop9@yandex.ru>
2025-02-27 14:06:52 +0700

    firstStartReactNative

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const App = () => {
+	return (
+		<View className="bg-black">
+			<Text> TUMBA UMBA</Text>
+		</View>
+	);
+};
+
+export default App;
