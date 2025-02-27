7ccf896a79d6bd23af42ae6e098c212a5aadfbff
Ray <nikanop9@yandex.ru>
2025-02-27 14:21:32 +0700

    gaf

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import "@/global.css";
 
 const App = () => {
 	return (
-		<View className="bg-black">
-			<Text> TUMBA UMBA</Text>
+		<View className="bg-black flex-1 text-4xl">
+			<Text> TUMBA UMBA ALOOOO</Text>
 		</View>
 	);
 };
